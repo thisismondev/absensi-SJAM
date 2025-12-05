24128b40dcf1f788205fcad4a34dfc8c429e0820
Raqhib <raqhib.ataillah@gmail.com>
2025-12-05 13:20:40 +0800

    create TextFieldCustom

---
+++ app/src/main/java/id/co/mondo/sjam/ui/component/TextFieldCustom.kt
@@ -0,0 +1,107 @@
+package id.co.mondo.sjam.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.co.mondo.sjam.R
+
+@Composable
+fun TextFieldCustom(
+    modifier: Modifier = Modifier,
+    values: String,
+    label: String,
+    onValueChange: (String) -> Unit,
+    isPasswordField: Boolean = false,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    enabled: Boolean = true
+){
+
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = values,
+        onValueChange = {
+            onValueChange(it)
+        },
+        label = {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.titleMedium
+            )
+        },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = Color.LightGray,
+            focusedLabelColor = MaterialTheme.colorScheme.primary,
+            cursorColor = MaterialTheme.colorScheme.primary
+        ),
+
+        modifier = modifier,
+        singleLine = true,
+        textStyle = MaterialTheme.typography.titleMedium,
+        trailingIcon = {
+            if (isPasswordField) {
+                IconButton(
+                    onClick = { isPasswordVisible = !isPasswordVisible }
+                ) {
+                    Icon(
+                        painter = painterResource(
+                            if (isPasswordVisible)
+                                R.drawable.visibilityon
+                            else
+                                R.drawable.visibilityoff
+                        ),
+                        contentDescription = "Toggle Password Visibility",
+                        tint = Color.LightGray
+                    )
+                }
+            }
+        },
+        visualTransformation = if(isPasswordField && !isPasswordVisible) PasswordVisualTransformation() else VisualTransformation.None,
+        shape = RoundedCornerShape(14.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        enabled = enabled,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTextField(){
+    Column {
+        TextFieldCustom(
+            values = "",
+            label = "Email",
+            onValueChange = {},
+            isPasswordField = false,
+            keyboardType = KeyboardType.Email
+        )
+        TextFieldCustom(
+            values = "",
+            label = "Password",
+            onValueChange = {},
+            isPasswordField = true,
+            keyboardType = KeyboardType.Password,
+
+        )
+    }
+}
+
